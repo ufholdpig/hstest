60c9bb6b012cb5616b5a3a169cd78086b45062cf
Allen <allen@allen-linux.(none)>
2013-07-20 17:07:33 -0400

    ok

--- app/models/UrlShortener.scala
+++ app/models/UrlShortener.scala
@@ -21,7 +21,7 @@ object UrlShortener {
       val m = ((x.toList.head & 0x3F) :: x.toList.tail).map("%02x".format(_)).mkString
       val n = Integer.parseInt(m, 16)
       (0 to 5).map { i => dict((n >> i*5) & 0x3d) }.mkString
-    }.mkString("++++++++++")
+    }.mkString("******")
   }
 
   def urltoLong(s: String): String = {
